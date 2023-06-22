f79e388d55e76b7bd3b5c5fe9b1ac5ec6045d3d9
Lucas B Ribeiro <br.lucasribeiro@gmail.com>
2023-06-21 23:55:14 -0300

    New Player Sprite

---
+++ player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="player" tilewidth="16" tileheight="16" tilecount="3" columns="3">
+ <image source="player.png" width="48" height="16"/>
+</tileset>
